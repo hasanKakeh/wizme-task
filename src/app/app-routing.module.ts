ef29766b6e1097ae23ff436eddcdfdf5a7b4c29d
hasanKakeh <hasan.kakeh1@gmail.com>
2023-08-12 00:16:17 +0300

    add redirect to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { NgModule } from '@angular/core';
 
-const routes: Routes = [{ path: '', loadChildren: () => import('@feature/user/user.module').then(m => m.UserModule) }];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('@feature/user/user.module').then(m => m.UserModule) },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
