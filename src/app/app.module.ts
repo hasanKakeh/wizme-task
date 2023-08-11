628c3a1a4db43ff23537ca4a94744f635dc0566d
hasanKakeh <hasan.kakeh1@gmail.com>
2023-08-12 00:32:43 +0300

    update core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
-import { UsersEffects, userReducer } from '@core/store';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { EffectsModule } from '@ngrx/effects';
+import { CoreModule } from '@core/core.module';
 import { LayoutModule } from './layout/layout.module';
 import { NgModule } from '@angular/core';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { StoreModule } from '@ngrx/store';
-import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,13 +15,7 @@ import { environment } from '../environments/environment';
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
-    StoreModule.forRoot({'users':userReducer}),
-    EffectsModule.forRoot([UsersEffects]),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25, // Retains last 25 states
-      logOnly: environment.production, // Restrict extension to log-only mode
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-    }),
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,23 @@
+import { UsersEffects, userReducer } from './store';
+
 import { CommonModule } from '@angular/common';
+import { EffectsModule } from '@ngrx/effects';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { NgModule } from '@angular/core';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [],
   imports: [
+    StoreModule.forRoot({'users':userReducer}),
+    EffectsModule.forRoot([UsersEffects]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
     CommonModule
   ],
   providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } }]
